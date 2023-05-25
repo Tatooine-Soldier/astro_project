dcef1db47ca2a5587e266d522bc4c804e831ad0c
Tom O <58118455+Tatooine-Soldier@users.noreply.github.com>
2023-05-25 18:35:34 +0100

    Add files via upload

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
                     <section class="home-divs-container">
                         <div class="home-divs">
                             Today's News
-                            <div>{{ titleList }}</div>
+                            <div></div>
                         </div>
                         <div class="home-divs">Images</div>
                         <div class="home-divs">News</div>
@@ -104,28 +104,3 @@ p {
 
 </style>
 
-<script>
-import { scrapeWeb } from "../scraper.js"
-
-    export default {
-        data() {
-            return {
-              titleList: [],
-              contentList: [],
-            }
-        }, 
-        mounted() {
-            var dlist = scrapeWeb()
-            console.log("dlist:", dlist)
-            dlist.then((data) => {
-                this.titleList = data.list.list
-                this.contentList = data.contentList.list
-                
-                console.log("data:", this.contentList)
-
-            
-            })
-           
-        }
-    }
-</script>
